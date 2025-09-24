94b7151f5eec1f158f081d68970d61f127e8b815
samuelyhsu <samuel.hsu@qq.com>
2025-09-24 20:44:54 +0800

    ch5 - 增加NEARBY14

--- src/ch5/gridnn.hpp
+++ src/ch5/gridnn.hpp
@@ -32,7 +32,8 @@ class GridNN {
         NEARBY8,  // 上下左右+四角
 
         // for 3D
-        NEARBY6,  // 上下左右前后
+        NEARBY6,   // 上下左右前后
+        NEARBY14,  // 上下左右前后+8个角
     };
 
     /**
@@ -135,6 +136,17 @@ void GridNN<3>::GenerateNearbyGrids() {
     } else if (nearby_type_ == NearbyType::NEARBY6) {
         nearby_grids_ = {KeyType(0, 0, 0),  KeyType(-1, 0, 0), KeyType(1, 0, 0), KeyType(0, 1, 0),
                          KeyType(0, -1, 0), KeyType(0, 0, -1), KeyType(0, 0, 1)};
+    } else if (nearby_type_ == NearbyType::NEARBY14) {
+        nearby_grids_ = {KeyType(0, 0, 0),
+                         // 左右
+                         KeyType(-1, 0, 0), KeyType(1, 0, 0),
+                         // 前后
+                         KeyType(0, 1, 0), KeyType(0, -1, 0),
+                         // 上下
+                         KeyType(0, 0, -1), KeyType(0, 0, 1),
+                         // 八个角
+                         KeyType(-1, -1, -1), KeyType(-1, 1, -1), KeyType(-1, -1, 1), KeyType(-1, 1, 1),
+                         KeyType(1, -1, -1), KeyType(1, 1, -1), KeyType(1, -1, 1), KeyType(1, 1, 1)};
     }
 }
 
